77dbb4c828b0d20ca031b2df1a044ace8ddd85ce
Marianna <marianna.albrici@gmail.com>
2023-07-31 14:29:36 +0200

    Modified imports

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-scroll';
-import myLogo from '/Users/marialbrici/Desktop/Personal-Portfolio/src/assets/img/creatorLogo.png';
+import myLogo from '../img/creatorLogo.png';
 
 /*--------------------
 * Header Menu
